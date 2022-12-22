1ddcdefdc393276dcf71fe49f64802615a4c9736
justinkim2015 <justinkim2015@gmail.com>
2022-12-22 14:57:53 +0900

    generate navbar with js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
-import './styles.css' 
-import './reset.css' 
+import './styles.css';
+import './reset.css'; 
+import _ from 'lodash';
+
+let content = document.getElementById('content')
+
+const buttonFactory = (text, id) => {
+  let div = document.createElement('div');
+
+  div.textContent = text
+  div.classList.add('tab')
+  div.setAttribute('id', `page${id}`)
+  
+  return div
+};
+
+function makeNavbar() {
+  let navbar = document.createElement('div');
+  navbar.setAttribute('id', 'navbar')
+  navbar.appendChild(buttonFactory('Home', 1))
+  navbar.appendChild(buttonFactory('Menu', 2))
+  navbar.appendChild(buttonFactory('Contact', 3))
+  return navbar
+}
+
+console.log(buttonFactory('hello', 1))
+
+content.appendChild(makeNavbar())
